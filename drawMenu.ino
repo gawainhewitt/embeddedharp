5c5f31bfbcb754ab4235188c71c8ff4025db22f2
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-09-27 13:02:22 +0100

    changed menu options

--- drawMenu.ino
+++ drawMenu.ino
@@ -19,22 +19,20 @@ void draw() {
     u8g2.setFont(u8g2_font_6x13_tr);
     
     u8g2.setCursor(1, 10);
-    u8g2.print("Scale: "); 
+    u8g2.print("Key: "); 
     u8g2.print("C"); 
-    u8g2.print(" "); 
-    u8g2.print("Major");
     
     u8g2.setCursor(1, 20);
-    u8g2.print("Notes: "); u8g2.print(6);
+    
 
     u8g2.setCursor(1, 30);
-    u8g2.print("Min Dist(cm): "); u8g2.print(12);
+    u8g2.print("Type: "); u8g2.print("Scale");
 
     u8g2.setCursor(1, 40);
-    u8g2.print("Max Dist(cm): "); u8g2.print(32);
+    
 
     u8g2.setCursor(1, 50);
-    u8g2.print("Inst: "); u8g2.print(32); u8g2.print("  Bank: "); u8g2.print(4);
+    
 
     u8g2.setCursor(1, 60);
     u8g2.print("Volume: "); u8g2.print(34);
